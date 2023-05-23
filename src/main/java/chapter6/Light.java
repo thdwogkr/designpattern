ba521dd1a4b1e29922654ab4f21558f2f757949a
sjhsjh1990 <sjhsjh1990@gmail.com>
2023-05-23 15:27:43 +0900

    [feat] chapter6 - LightOffCommand 생성

--- src/main/java/chapter6/Light.java
+++ src/main/java/chapter6/Light.java
@@ -3,6 +3,10 @@ package src.main.java.chapter6;
 public class Light {
 
     public void on() {
-        System.out.println("Light on");
+        System.out.println("Light on!!!");
+    }
+
+    public void off() {
+        System.out.println("Light off!!!");
     }
 }
