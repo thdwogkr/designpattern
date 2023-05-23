9256de44c3569fb4229560a9fb01c7181addac92
sjhsjh1990 <sjhsjh1990@gmail.com>
2023-05-23 15:28:02 +0900

    [feat] chapter6 - Stereo 관련 class 생성

---
+++ src/main/java/chapter6/Stereo.java
@@ -0,0 +1,21 @@
+package src.main.java.chapter6;
+
+public class Stereo {
+    public void on() {
+        System.out.println("Stereo On!!!");
+    }
+
+    public void setCD() {
+        System.out.println("Set CD!!!");
+    }
+
+    public void setVolume(int volume) {
+        System.out.println("volume이 " + volume + " 입니다");
+    }
+
+    public void off() {
+        System.out.println("Stereo Off!!");
+    }
+
+
+}
---
+++ src/main/java/chapter6/StereoOffCommand.java
@@ -0,0 +1,13 @@
+package src.main.java.chapter6;
+
+public class StereoOffCommand implements Command{
+    Stereo stereo;
+
+    public StereoOffCommand(Stereo stereo) {
+        this.stereo = stereo;
+    }
+
+    public void execute() {
+        stereo.off();
+    }
+}
