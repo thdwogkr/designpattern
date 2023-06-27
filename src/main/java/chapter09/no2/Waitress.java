5ef8ef3425c189e41f4d2c79849fc47909b83ed8
sjhsjh1990 <sjhsjh1990@gmail.com>
2023-06-28 07:45:04 +0900

    [feat] chapter9

---
+++ src/main/java/chapter09/no2/Waitress.java
@@ -0,0 +1,13 @@
+package src.main.java.chapter09.no2;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
